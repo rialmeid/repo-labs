c4ee84e2a02527092a89336ad785c30a6eb335d3
Ricardo <ricardo@carrietech.com.br>
2021-11-19 00:09:51 -0300

    usando reduce

--- RicardoLabs/src/test/java/com/carrietech/ricardolabs/stream/MapTest.java
+++ RicardoLabs/src/test/java/com/carrietech/ricardolabs/stream/MapTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,5 +28,12 @@ class MapTest {
         List<String> collect = stream.map(s -> s.concat(" Almeida")).collect(Collectors.toList());
         boolean almeida = collect.stream().allMatch(s -> s.contains("Almeida"));
         Assertions.assertTrue(almeida);
+    }
+
+    @Test
+    void reduce_withValues_thenSum() {
+        Stream<Double> stream = Stream.of(100d, 200d, 300d);
+        Double reduce = stream.reduce(0d, (prim, segun) -> prim + segun);
+        System.out.println(reduce);
     }
 }
